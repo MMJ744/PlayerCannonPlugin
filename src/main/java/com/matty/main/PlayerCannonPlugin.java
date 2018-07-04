40f40d54584570ae124d80308f72e4f2f6525da9
MMJ744 <MMJ744@cs.bham.ac.uk>
2018-07-04 14:04:31 +0100

    initial files no code

---
+++ src/main/java/com/matty/main/PlayerCannonPlugin.java
@@ -0,0 +1,11 @@
+package com.matty.main;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PlayerCannonPlugin extends JavaPlugin{
+	@Override
+	public void onEnable() {
+		
+		getLogger().info("Cannon Enabled");
+	}
+}
